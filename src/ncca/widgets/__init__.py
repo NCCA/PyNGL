e89f03ef94de56d9aeb9f3d00c3958a4991efbd6
Jon Macey <jmacey@bournemouth.ac.uk>
2025-12-02 12:11:14 +0000

    added new widgets

---
+++ src/ncca/widgets/__init__.py
@@ -0,0 +1,9 @@
+__version__ = "0.1.0"
+
+
+from .transformwidget import TransformWidget
+from .vec2widget import Vec2Widget
+from .vec3widget import Vec3Widget
+from .vec4widget import Vec4Widget
+
+__all__ = ["Vec2Widget", "Vec3Widget", "Vec4Widget", "TransformWidget"]
---
+++ src/ncca/widgets/__main__.py
@@ -0,0 +1,61 @@
+import sys
+
+from PySide6.QtWidgets import QApplication, QDialog, QGridLayout, QLabel
+
+from ncca.ngl import Vec2, Vec3, Vec4
+from ncca.widgets import TransformWidget, Vec2Widget, Vec3Widget, Vec4Widget
+
+
+class SimpleDialog(QDialog):
+    def __init__(self, parent=None):
+        super(SimpleDialog, self).__init__(parent)
+        self.setWindowTitle("PyNGL ncca.widgets library ")
+        self.setMinimumWidth(200)
+        layout = QGridLayout()
+
+        self.vec2_widget = Vec2Widget("Vec2 Widget", Vec2(1, 2), self)
+        self.vec2_widget.set_x_range(-1, 1)
+        self.vec2_widget.set_y_range(-2, 2)
+        layout.addWidget(self.vec2_widget, 0, 0)
+        self.vec2_label = QLabel("[0.0,0.0]")
+        layout.addWidget(self.vec2_label, 0, 1)
+        self.vec2_widget.valueChanged.connect(self._update_vec2)
+
+        self.vec3_widget = Vec3Widget("Vec3 Widget", Vec3(1, 2, 3), self)
+        self.vec3_widget.set_y_range(-2, 2)
+        self.vec3_widget.set_z_range(-3, 3)
+        layout.addWidget(self.vec3_widget, 1, 0)
+        self.vec3_label = QLabel("[0.0,0.0,0.0]")
+        layout.addWidget(self.vec3_label, 1, 1)
+        self.vec3_widget.valueChanged.connect(self._update_vec3)
+
+        self.vec4_widget = Vec4Widget("Vec4 Widget", Vec4(1, 2, 3, 1.0), self)
+        self.vec4_widget.set_y_range(-2, 2)
+        self.vec4_widget.set_z_range(-3, 3)
+        layout.addWidget(self.vec4_widget, 2, 0)
+        self.vec4_label = QLabel("[0.0,0.0,0.0,1.0]")
+        layout.addWidget(self.vec4_label, 2, 1)
+        self.vec4_widget.valueChanged.connect(self._update_vec4)
+
+        self.transform_widget = TransformWidget("Transform Widget", self)
+        layout.addWidget(self.transform_widget, 3, 0)
+
+        self.setLayout(layout)
+
+    def _update_vec3(self, value):
+        self.vec3_label.setText(f"[{value.x:0.2f}, {value.y:0.2f}, {value.z:0.2f}]")
+
+    def _update_vec2(self, value):
+        self.vec2_label.setText(f"[{value.x:0.2f}, {value.y:0.2f}]")
+
+    def _update_vec4(self, value):
+        self.vec4_label.setText(f"[{value.x:0.2f}, {value.y:0.2f}, {value.z:0.2f}, {value.w:0.2f}]")
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    print("NCCA Widgets")
+
+    dialog = SimpleDialog()
+    dialog.show()
+    app.exec()
