a56b11ffbed37075b875809c108815931f2e5f69
Jon Macey <jmacey@bournemouth.ac.uk>
2026-07-08 07:55:15 +0100

    feat(qml): add TransformModel
    Composes position/rotation/scale Vec3Model children and computes a Mat4
    via ncca.ngl.Transform, exposing rotationOrderIndex, matrix, and
    matrix_text() for QML consumption.

--- src/ncca/ngl/qml/__init__.py
+++ src/ncca/ngl/qml/__init__.py
@@ -13,6 +13,7 @@ __license__ = "MIT"
 from .mat2_model import Mat2Model
 from .mat3_model import Mat3Model
 from .mat4_model import Mat4Model
+from .transform_model import TransformModel
 from .vec2_model import Vec2Model
 from .vec3_model import Vec3Model
 from .vec4_model import Vec4Model
@@ -24,4 +25,5 @@ __all__ = [
     "Mat2Model",
     "Mat3Model",
     "Mat4Model",
+    "TransformModel",
 ]
